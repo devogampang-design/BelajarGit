25ea7bcb19cb0f73e89db5d5c143f4a15153562c
NotAndrew67 <andrewnih57@gmail.com>
2026-04-27 08:37:53 +0700

    fix: implement pause and resume state

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,38 +1,60 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-
     public GameState currentState;
 
     void Awake()
     {
-        Instance = this;
+        if (Instance == null) { Instance = this; DontDestroyOnLoad(gameObject); }
+        else { Destroy(gameObject); }
     }
 
     void Start()
     {
-        currentState = GameState.Playing;
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            PauseGame();
-        }
+        if (SceneManager.GetActiveScene().name != "MainMenu")
+            currentState = GameState.Playing;
+        else
+            currentState = GameState.MainMenu;
     }
 
     public void PauseGame()
     {
-        Time.timeScale = 0f;
         currentState = GameState.Paused;
+        Time.timeScale = 0f;
+
+        Object.FindFirstObjectByType<UIManager>()?.TogglePausePanel(true);
+    }
+
+    public void ResumeGame()
+    {
+        currentState = GameState.Playing;
+        Time.timeScale = 1f;
+
+        Object.FindFirstObjectByType<UIManager>()?.TogglePausePanel(false);
+        Debug.Log("Panel Pause dimatikan, Game Jalan Lagi");
     }
 
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        if (currentState == GameState.GameOver) return;
+
         currentState = GameState.GameOver;
+        Time.timeScale = 0f;
+
+        Object.FindFirstObjectByType<UIManager>()?.ToggleGameOverPanel(true);
+    }
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
